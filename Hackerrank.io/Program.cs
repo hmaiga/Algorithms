201c2d9d12b784146db9e4af22e8a10bfa06ddbe
MAIGA Housseini Daouda <fouss.maiga@hotmail.fr>
2016-10-09 16:29:52 +0200

    Added some methods in both projects

--- Hackerrank.io/Program.cs
+++ Hackerrank.io/Program.cs
@@ -1,12 +1,124 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Hackerrank.io
 {
     class Program
     {
+        protected static int SequentialSearch(int[] flag, int e)
+        {
+            var arrayLength = flag.Length;
+            var index = 0;
+            do
+            {
+                index++;
+            } while (index < flag.Length && flag[index] != e);
+            if (index < arrayLength)
+            {
+                return index;
+            }
+
+            return -1;
+        }
+
+        protected static int BinarySearch(int[] flag, int e)
+        {
+            //Diviser pour mieux regner
+            var leftSide = 0;
+            var rightSide = flag.Length;
+            var index = (rightSide + leftSide) / 2;
+            do
+            {
+                if (e < flag[index])
+                {
+                    rightSide = index - 1;
+                }
+                else
+                {
+                    leftSide = index + 1;
+                }
+                index = (rightSide + leftSide) / 2;
+            } while (e != flag[index] && leftSide <= rightSide);
+
+            if (e == flag[index])
+            {
+                return index;
+            }
+            return -1;
+        }
+
+        private static int Partition(int[] myArray, int leftSide, int rightSide)
+        {
+            int pivot = myArray[rightSide];
+            int i = leftSide - 1;
+            for (int j = leftSide; j < rightSide; j++)
+            {
+                if (myArray[j] <= pivot)
+                {
+                    i++;
+                    int tmp = myArray[i];
+                    myArray[i] = myArray[j];
+                    myArray[j] = tmp;
+                }
+            }
+            int index = i + 1;
+            int flag = myArray[index];
+            myArray[index] = myArray[rightSide];
+            myArray[rightSide] = flag;
+
+            return index;
+        }
+
+        protected static int[] QuickSort(int[] myArray, int leftSide, int rightSide)
+        {
+            if (leftSide < rightSide)
+            {
+                int pivot = Partition(myArray, leftSide, rightSide);
+                QuickSort(myArray, leftSide, pivot - 1);
+                QuickSort(myArray, pivot + 1, rightSide);
+            }
+
+            return myArray;
+        }
+
+        public static int GetMatchedCouples(int[] myArray, int n, int k)
+        {
+            int count = 0;
+            for (var i = 0; i < n; i++)
+            {
+                for (int j = i; j < n; j++)
+                {
+                    if ((myArray[i] + myArray[j]) / k == 0)
+                    {
+                        count++;
+                    }
+                }
+            }
+            return count;
+        }
+
+        private static Tuple<DateTime, DateTime> GetDay(DateTime currentDate)
+        {
+            var map = new Dictionary<System.DayOfWeek, Tuple<DateTime, DateTime>>()
+            {
+                {System.DayOfWeek.Monday, new Tuple<DateTime, DateTime>(currentDate, currentDate.AddDays(6)) },
+                {System.DayOfWeek.Tuesday, new Tuple<DateTime, DateTime>(currentDate.AddDays(-1), currentDate.AddDays(5)) },
+                {System.DayOfWeek.Wednesday, new Tuple<DateTime, DateTime>(currentDate.AddDays(-2), currentDate.AddDays(4))},
+                {System.DayOfWeek.Thursday, new Tuple<DateTime, DateTime>(currentDate.AddDays(-3), currentDate.AddDays(3)) },
+                {System.DayOfWeek.Friday, new Tuple<DateTime, DateTime>(currentDate.AddDays(-4), currentDate.AddDays(2)) },
+                {System.DayOfWeek.Saturday,  new Tuple<DateTime, DateTime>(currentDate.AddDays(-5), currentDate.AddDays(1)) },
+                {System.DayOfWeek.Sunday, new Tuple<DateTime, DateTime>(currentDate.AddDays(-6), currentDate) },
+            };
+            Tuple<DateTime, DateTime> output;
+            map.TryGetValue(currentDate.DayOfWeek, out output);
+            return output;
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
+            int[] myArray = new[] {10, 20, 20, 10, 10, 30, 50, 10, 20};
+
+            Console.ReadKey();
         }
     }
 }
--- ProjectEuler/Program.cs
+++ ProjectEuler/Program.cs
@@ -1,9 +1,169 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace ProjectEuler
 {
     class Program
     {
+        private static void Multiple(int n)
+        {
+
+            var count = 0;
+            for (var i = 1; i < n + 1; i++)
+            {
+                if (i % 3 == 0 || i % 5 == 0 && i % 15 != 0)
+                {
+                    count += i;
+                }
+            }
+            Console.WriteLine(count - n);
+        }
+
+        private static int Fibonacci(int n)
+        {
+            if (n < 0 || n > 46)
+            {
+                throw new ArgumentException();
+            }
+            if (n == 1 || n == 2)
+            {
+                return n;
+            }
+
+            return Fibonacci(n - 1) + Fibonacci(n - 2);
+        }
+
+        private static double LargestPrimeFactor(double n)
+        {
+            //int primeFactor = 2;
+            //n=28
+            for (var i = 2; i < Math.Sqrt(n); i++)
+            {
+                while (n % i == 0)
+                {
+                    n = n / 2;
+                }
+            }
+
+            return n;
+        }
+
+        private static void PrimeFactor(long n)
+        {
+            var q = Math.Ceiling(Math.Sqrt(n));
+            var a = q + 1;
+            double b = 0;
+            var bCarre = Math.Pow(a, 2) - n;
+            var deuxAplusUn = 2 * a + 1;
+            var cpt = 0;
+            while (Math.Abs(Math.Pow((int)Math.Sqrt(bCarre), 2) - bCarre) != 0)
+            {
+                bCarre += deuxAplusUn;
+                deuxAplusUn = deuxAplusUn + 2;
+                a++;
+                cpt++;
+            }
+            b = Math.Sqrt(bCarre);
+            Console.WriteLine($"Le nombre {n} est le produit de {a + b} par {a - b}.");
+            Console.WriteLine($"Et il a fallu {cpt} étapes pour le factoriser.");
+        }
+
+        private static Tuple<double, double> SumSquareDifference(int n)
+        {
+            //25164150
+            double counter = 0;
+            double optimizeCounter = 0;
+            double sumSquare = 0;
+            double squareSum = 0;
+            for (var i = 1; i <= n; i++)
+            {
+                sumSquare += Math.Pow(i, 2);
+                //Console.WriteLine(Math.Pow(i, 2));
+                squareSum += i;
+                //Console.WriteLine(squareSum);
+            }
+            counter = Math.Pow(squareSum, 2) - sumSquare;
+            optimizeCounter = Math.Pow(n, 2) * Math.Pow(n + 1, 2) * 1 / 4 + n * (n + 1) * (2 * n + 1) * 1 / 6;
+            return new Tuple<double, double>(counter, optimizeCounter);
+        }
+
+        private static double SmallestMultiple(int n)
+        {
+            double counter = 0;
+            var x = 0;
+            for (var i = 1; i <= x++; i++)
+            {
+                for (var j = 1; j <= n; j++)
+                {
+                    if (x % j == 0)
+                    {
+                        counter = x;
+                    }
+                }
+            }
+            return counter;
+        }
+
+        public static int[] GeneratePrime(int limit)
+        {
+            var primes = new List<int>();
+            bool isPrime;
+            var j = 0;
+
+            primes.Add(2);
+            for (var i = 3; i <= limit; i++)
+            {
+                j = 0;
+                isPrime = true;
+                while (Math.Pow(primes[j], 2) <= i)
+                {
+                    if (i % primes[j] == 0)
+                    {
+                        isPrime = false;
+                        break;
+                    }
+                    j++;
+                }
+                if (isPrime)
+                {
+                    primes.Add(i);
+                }
+            }
+            return primes.ToArray<int>();
+        }
+
+        private static double SmallestPrimeFactor(int n)
+        {
+            var primeFactor = new List<int> { 2, 3, 5, 7, 11, 13, 17, 19, 23 };
+            var stockPrimeFactor = new List<int>();
+            double counter = 0;
+            for (var i = 2; i <= n; i++)
+            {
+                //i = 3
+                var flag = 0;
+                var j = 0;
+                while (i >= 2)
+                {
+                    if (i % 2 == 0 && i != 0)
+                    {
+                        flag = i / 2;
+                        j++;
+                    }
+                    else
+                    {
+                        var casting = i.ToString();
+                        for (var k = 0; k < casting.Length; k++)
+                        {
+                            var x = int.Parse(casting[k].ToString());
+                        }
+                    }
+                }
+            }
+
+            return counter;
+        }
+
         static void Main(string[] args)
         {
         }
